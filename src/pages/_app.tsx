71fae964daac663263f6c9a7c441483a312ca91a
mudaston <twil.revaas@gmail.com>
2023-04-23 15:46:28 +0300

    add device definition and pass prop 'mobile' to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,41 @@
 import type { AppProps } from 'next/app'
+import App from 'next/app'
 import { Provider } from 'react-redux'
 
+import { getUserAgent } from '@app/device-detect'
+import { isMobile } from '@app/device-detect/mobile'
 import { wrapper } from '@app/store'
 import RootLayout from '@layouts/root-layout'
 
 import '@app/styles/scss/main.scss'
 
-function App({ Component, ...rest }: AppProps) {
+function MyApp({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest)
+  const { pageProps } = props
 
   return (
     <Provider store={store}>
       <RootLayout>
-        <Component {...props.pageProps} />
+        <Component {...pageProps} />
       </RootLayout>
     </Provider>
   )
 }
 
-export default App
+MyApp.getInitialProps = async (ctx: any) => {
+  const initialProps = await App.getInitialProps(ctx)
+  const { rawHeaders } = ctx.ctx.req
+
+  const userAgent = getUserAgent(rawHeaders)
+  const mobile = isMobile(userAgent)
+
+  return {
+    ...initialProps,
+    pageProps: {
+      // pass prop to all pages
+      mobile,
+    },
+  }
+}
+
+export default MyApp
