a63ef0c4cbfbf38053d46c55c3c1430afe8649c7
mudaston <twil.revaas@gmail.com>
2023-04-23 21:12:21 +0300

    return empty string if user agent not found

--- src/app/device-detect/index.ts
+++ src/app/device-detect/index.ts
@@ -1,6 +1,15 @@
 const findUserAgentIndex = (headers: Array<string>) =>
   headers.findIndex((header: string) => header === 'User-Agent')
 
-const getUserAgent = (headers: Array<string>) => headers[findUserAgentIndex(headers) + 1]
+const getUserAgent = (headers: Array<string>) => {
+  const index = findUserAgentIndex(headers)
+  const notFound = -1
+
+  if (index === notFound) {
+    return ''
+  }
+
+  return headers[findUserAgentIndex(headers) + 1]
+}
 
 export { getUserAgent }
