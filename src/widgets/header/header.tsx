024e13a8d122946b4e2fafd8a77b071ce215c6fb
mudaston <twil.revaas@gmail.com>
2023-04-28 13:56:56 +0300

    create "header" widget

---
+++ src/widgets/header/header.tsx
@@ -0,0 +1,33 @@
+import dynamic from 'next/dynamic'
+import React, { FC } from 'react'
+
+const MobileMenu = dynamic(() =>
+  import('@shared/ui/mobile-menu').then(({ MobileMenu }) => MobileMenu)
+)
+const DesktopMenu = dynamic(() =>
+  import('@shared/ui/desktop-menu').then(({ DesktopMenu }) => DesktopMenu)
+)
+
+interface HeaderProps {
+  mobile: boolean
+}
+
+type Props = HeaderProps
+
+const header: FC<Props> = ({ mobile }) => {
+  const DesktopView = !mobile && <DesktopMenu />
+  const MobileView = mobile && <MobileMenu />
+
+  return (
+    <>
+      {DesktopView}
+      {MobileView}
+    </>
+  )
+}
+
+header.defaultProps = {
+  mobile: false,
+}
+
+export default header
